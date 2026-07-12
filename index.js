dbab17ee9015ddcb050ca1444f7b71839e0ffcb8
Zayro2 <bertrandjordan500@gmail.com>
2026-07-13 00:50:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,62 @@
 require("dotenv").config();
 
 const {
-    Client,
-    GatewayIntentBits,
-    REST,
-    Routes,
-    SlashCommandBuilder,
-    ChannelType
+Client,
+GatewayIntentBits,
+REST,
+Routes
 } = require("discord.js");
 
 
+const suggestion = require("./commands/suggestion");
+
+
 const client = new Client({
-    intents: [
+    intents:[
         GatewayIntentBits.Guilds
     ]
 });
 
 
-// Commandes du bot
-const commands = [
-    new SlashCommandBuilder()
-        .setName("suggestion")
-        .setDescription("Créer une nouvelle proposition")
-        .addStringOption(option =>
-            option
-                .setName("titre")
-                .setDescription("Titre de la proposition")
-                .setRequired(true)
-        )
-        .addStringOption(option =>
-            option
-                .setName("description")
-                .setDescription("Description de la proposition")
-                .setRequired(true)
-        )
-].map(command => command.toJSON());
+client.once("ready", async()=>{
+
+console.log(`🏛️ Connecté : ${client.user.tag}`);
 
 
-// Installation des commandes
-const rest = new REST({ version: "10" })
-    .setToken(process.env.TOKEN);
+const rest = new REST({version:"10"})
+.setToken(process.env.TOKEN);
 
 
-client.once("ready", async () => {
-
-    console.log(`🏛️ Architecte connecté : ${client.user.tag}`);
-
-
-    await rest.put(
-        Routes.applicationCommands(process.env.CLIENT_ID),
-        {
-            body: commands
-        }
-    );
-
-
-    console.log("✅ Commande /suggestion activée");
+await rest.put(
+Routes.applicationCommands(process.env.CLIENT_ID),
+{
+body:[
+suggestion.data
+]
 });
 
 
-// Création d'une suggestion
-client.on("interactionCreate", async interaction => {
+console.log("✅ Commandes chargées");
 
-    if (!interaction.isChatInputCommand()) return;
-
-
-    if (interaction.commandName === "suggestion") {
+});
 
 
-        const titre = interaction.options.getString("titre");
-        const description = interaction.options.getString("description");
+
+client.on(
+"interactionCreate",
+async interaction=>{
 
 
-        const salon = await interaction.guild.channels.create({
+if(
+interaction.isChatInputCommand()
+&& interaction.commandName==="suggestion"
+){
 
-            name: `📜-proposition-${Date.now().toString().slice(-4)}`,
+suggestion.execute(interaction);
 
-            type: ChannelType.GuildText
-
-        });
-
-
-        await salon.send({
-
-            content:
-`🏛️ **Nouvelle proposition**
-
-👤 Auteur : ${interaction.user}
-
-💡 **Titre :**
-${titre}
-
-📝 **Description :**
-${description}
-
-🗳️ Les votes arriveront bientôt.`
-
-        });
+}
 
 
-        await interaction.reply({
-
-            content: `✅ Ta proposition a été créée : ${salon}`,
-
-            ephemeral: true
-
-        });
-
-    }
 
 });
 
